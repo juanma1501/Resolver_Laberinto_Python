3a5af6a2bb770fc377c517f1b56d1dc6d854244a
José Carlos Gualo Cejudo <72818493+JoseCarlosGualo@users.noreply.github.com>
2020-11-09 17:45:43 +0100

    Add files via upload
    Code for the class State

---
+++ State.py
@@ -0,0 +1,30 @@
+#Let be a state as a cell of the maze with all its information,
+# its value and its neighbours.
+# The unique identifier for a state is the cell position (row, col).
+class State:
+
+    def __init__(self, row, column, node, neighbors):
+        self.id = (row, column)
+        self.node = node
+        self.neighbors = sorted(neighbors)
+
+    #Function to get the node
+    #associated with the state
+    def getNode(self):
+        return self.node
+
+    # Function to get the identifier
+    def getId(self):
+        return self.id
+
+    # Function to get the list of neighbours
+    def getNeighbors(self):
+        return self.neighbors
+
+    # Function to get the row of the cell
+    def getRow(self):
+        return self.id[0]
+
+    # Function to get the column of the cell
+    def getColumn(self):
+        return self.id[1]
